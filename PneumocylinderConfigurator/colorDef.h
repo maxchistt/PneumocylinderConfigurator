1cf7a3447e39528c6e7640a7c8b756862c3e503c
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2022-05-24 12:28:58 +0300

    colorDef upd

--- PneumocylinderConfigurator/colorDef.h
+++ PneumocylinderConfigurator/colorDef.h
@@ -1,42 +1,23 @@
 #pragma once
+#include <vsn_vision.h>
+#include <vsn_color.h>
 
 #define RGB(r,g,b)          ((COLORREF)(((BYTE)(r)|((WORD)((BYTE)(g))<<8))|(((DWORD)(BYTE)(b))<<16)))
 
-#define BLACK        colorsDef[0]
-#define BLUE         colorsDef[1]
-#define GREEN        colorsDef[2]
-#define CYAN         colorsDef[3]
-#define RED          colorsDef[4]
-#define MAGENTA      colorsDef[5]
-#define BROWN        colorsDef[6]
-#define LIGHTGRAY    colorsDef[7]
-#define DARKGRAY     colorsDef[8]
-#define LIGHTBLUE    colorsDef[9]
-#define LIGHTGREEN   colorsDef[10]
-#define LIGHTCYAN    colorsDef[11]
-#define LIGHTRED     colorsDef[12]
-#define LIGHTMAGENTA colorsDef[13]
-#define YELLOW       colorsDef[14]
-#define WHITE        colorsDef[15]
-#define GOLDEN       colorsDef[16]
-
-// Цвета для стилей
-COLORREF colorsDef[] = {
-  RGB(0,   0,   0),
-  RGB(0,   0, 192),
-  RGB(0, 192,   0),
-  RGB(0, 128, 128),
-  RGB(192,   0,   0),
-  RGB(96,   0, 192),
-  RGB(192, 128,   0),
-  RGB(192, 192, 192),
-  RGB(128, 158, 158),
-  RGB(0,   0, 255),
-  RGB(0, 255,   0),
-  RGB(0, 125, 255),
-  RGB(255,   0,   0),
-  RGB(255,   0, 125),
-  RGB(128, 128,   0),
-  RGB(255, 255, 255),
-  RGB(222, 222, 125),
-};
+#define BLACK        RGB(0,   0,   0)
+#define BLUE         RGB(0,   0, 192)
+#define GREEN        RGB(0, 192,   0)
+#define CYAN         RGB(0, 128, 128)
+#define RED          RGB(192,   0,   0)
+#define MAGENTA      RGB(96,   0, 192)
+#define BROWN        RGB(192, 128,   0)
+#define LIGHTGRAY    RGB(192, 192, 192)
+#define DARKGRAY     RGB(128, 158, 158)
+#define LIGHTBLUE    RGB(0,   0, 255)
+#define LIGHTGREEN   RGB(0, 255,   0)
+#define LIGHTCYAN    RGB(0, 125, 255)
+#define LIGHTRED     RGB(255,   0,   0)
+#define LIGHTMAGENTA RGB(255,   0, 125)
+#define YELLOW       RGB(128, 128,   0)
+#define WHITE        RGB(255, 255, 255)
+#define GOLDEN       RGB(222, 222, 125)
