71347926c48746286dee53b636a68eeffd05a348
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2022-05-29 22:25:17 +0300

    new qt version

--- CreateInstaller/Deploy.bat
+++ CreateInstaller/Deploy.bat
@@ -1,7 +1,7 @@
 @echo "Deploying PneumocylinderConfigurator"
 
 :: Путь к папке с деплоером windeployqt.exe
-set Qt_DeployerDir=C:\Qt\5.15.0\msvc2019_64\bin
+set Qt_DeployerDir=C:\Qt\5.15.2\msvc2019_64\bin
 :: Путь к папке этого проекта, %cd% - текущая папка
 set PneumocylinderConfigurator_ProjectDir=%cd%\..
 
