199d2fecaa656f1f807575aa6eb62c6ea5c46a10
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2022-06-08 04:51:48 +0300

    Update Deploy.bat

--- CreateInstaller/Deploy.bat
+++ CreateInstaller/Deploy.bat
@@ -1,9 +1,9 @@
 @echo "Deploying PneumocylinderConfigurator"
 
-:: Путь к папке с деплоером windeployqt.exe
-set Qt_DeployerDir=C:\Qt\5.15.2\msvc2019_64\bin
+:: Путь к папке с деплоером windeployqt.exe, QtDir - переменная среды, должна быть заранее задана
+set Qt_BinDir=%QtDir%\bin
 :: Путь к папке этого проекта, %cd% - текущая папка
-set PneumocylinderConfigurator_ProjectDir=%cd%\..
+set PneumocylinderConfigurator_OutDir=%cd%\..\x64\Release
 
-cd "%Qt_DeployerDir%"
-start windeployqt.exe %PneumocylinderConfigurator_ProjectDir%\x64\Release\PneumocylinderConfigurator.exe
+cd "%Qt_BinDir%"
+start windeployqt.exe "%PneumocylinderConfigurator_OutDir%\PneumocylinderConfigurator.exe"
